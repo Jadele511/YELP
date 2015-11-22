359f6d03264fa363ccaae6f89958538cb9dfad58
Jadele511 <baongocle1991@gmail.com>
2015-11-23 00:18:41 +0700

    yelp

---
+++ Yelp/SortTCell.swift
@@ -0,0 +1,24 @@
+//
+//  SortTCell.swift
+//  Yelp
+//
+//  Created by USER on 11/22/15.
+//  Copyright © 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class SortTCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
